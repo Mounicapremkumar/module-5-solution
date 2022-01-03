b19fc95d752255f89939a85fc2805b0c57a4e531
Mounicapremkumar <74371824+Mounicapremkumar@users.noreply.github.com>
2022-01-03 14:34:22 +0530

    script.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var homeHtmlUrl = "snippets/home-snippet.html";
+var allCategoriesUrl = 
+  "http://mounica-restaurant.herokuapp.com/categories.json";
+  "https://mounica-restaurant.herokuapp.com/categories.json";
+var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+var categoryHtml = "snippets/category-snippet.html";
+var menuItemsUrl = 
+  "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+var menuItemsTitleHtml = "snippets/menu-items-title.html";
+var menuItemHtml = "snippets/menu-item.html";
